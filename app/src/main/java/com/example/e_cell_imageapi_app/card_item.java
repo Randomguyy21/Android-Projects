b7c1376c0d4ee585c720ff93c54301aae1d7d7a9
Hard-Coder05 <smishra1605@gmail.com>
2020-03-16 00:52:05 +0530

    card_layout added card_item.java added gradle file and manifest file updated

---
+++ app/src/main/java/com/example/e_cell_imageapi_app/card_item.java
@@ -0,0 +1,14 @@
+package com.example.e_cell_imageapi_app;
+
+public class card_item {
+    private String mImageUrl;
+
+    public card_item(String ImageUrl){
+        mImageUrl=ImageUrl;
+    }
+
+    public String getmImageUrl(){
+        return mImageUrl;
+    }
+    
+}
