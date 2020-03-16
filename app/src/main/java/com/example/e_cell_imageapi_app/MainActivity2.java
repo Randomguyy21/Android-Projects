e4af700a4db999a7749a19034510eda0522a003c
Hard-Coder05 <smishra1605@gmail.com>
2020-03-16 11:23:24 +0530

    Search Page UI done Final Commit

--- app/src/main/java/com/example/e_cell_imageapi_app/MainActivity2.java
+++ app/src/main/java/com/example/e_cell_imageapi_app/MainActivity2.java
@@ -1,31 +1,21 @@
 package com.example.e_cell_imageapi_app;
-
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-
+import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
-
     private EditText Name;
-    private Button Search;
-
-
+    private ImageView Search;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pagal);
-
         Name = (EditText)findViewById(R.id.a1);
-
-        Search = (Button)findViewById(R.id.a2);
-
-
-
+        Search = (ImageView) findViewById(R.id.a2);
         Search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,15 +23,12 @@ public class MainActivity2 extends AppCompatActivity {
             }
         });
     }
-
     private void validate(String userName){
         if(userName!=""){
             Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
             intent.putExtra("EXTRA_SESSION_ID", userName);
             startActivity(intent);
         }else{
-
         }
     }
-
 }
